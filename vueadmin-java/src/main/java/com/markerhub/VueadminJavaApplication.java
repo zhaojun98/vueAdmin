d42cafda2cb716aa61c57aa5fb29ef8cf9bedb8b
jerry <67642821+zhaojun98@users.noreply.github.com>
2021-09-23 14:54:25 +0800

    Update VueadminJavaApplication.java

--- vueadmin-java/src/main/java/com/markerhub/VueadminJavaApplication.java
+++ vueadmin-java/src/main/java/com/markerhub/VueadminJavaApplication.java
@@ -3,6 +3,7 @@ package com.markerhub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan(basePackages = {"com.yl.mapper"})
 @SpringBootApplication
 public class VueadminJavaApplication {
 
